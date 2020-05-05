008d0c6a3fac1094d8842b2c4a63e28f96d2f232
ar-arif <56508132+ar-arif@users.noreply.github.com>
2020-05-05 13:12:50 +0600

    Update main.js

--- main.js
+++ main.js
@@ -1,92 +1,93 @@
 // b{}[]``
 
-
 function getAllData() {
-  
-  
-  
-  
-  
+  fetch("https://api.covid19api.com/summary")
+    .then((apiData) => {
+      return apiData.json();
+    })
+    .then((data) => {
+      const coronaData = data;
+      runFetch(coronaData);
+    });
+
   function runFetch(coronaData) {
-  const global = coronaData.Global;
-  const countries = coronaData.Countries; 
-  
-  // console.log(coronaData);
-   console.log(global);
-   console.log(countries);
-  
-let tc =  document.getElementById('todayConfirm');
-let tcc = document.getElementById('totalConfirm');
+    const global = coronaData.Global;
+    const countries = coronaData.Countries;
 
-let td = document.getElementById('todayDeaths');
-let tdd = document.getElementById('totalDeaths');
+    // console.log(coronaData);
+    console.log(global);
+    console.log(countries);
 
-let tr = document.getElementById('todayRecovered');
-let trr = document.getElementById('totalRecovered');
+    let tc = document.getElementById("todayConfirm");
+    let tcc = document.getElementById("totalConfirm");
 
-tc.innerHTML = `New Confirmed : ${global.NewConfirmed}`
-tcc.innerHTML = `Total Confirmed : ${global.TotalConfirmed}`
+    let td = document.getElementById("todayDeaths");
+    let tdd = document.getElementById("totalDeaths");
 
-td.innerHTML = `New Deaths : ${global.NewDeaths}`
-tdd.innerHTML = `Total Deaths : ${global.TotalDeaths}`
+    let tr = document.getElementById("todayRecovered");
+    let trr = document.getElementById("totalRecovered");
 
-tr.innerHTML = `New Recovered : ${global.NewRecovered}`
-trr.innerHTML = `Total Recovered : ${global.TotalRecovered}`
+    tc.innerHTML = `New Confirmed : ${global.NewConfirmed}`;
+    tcc.innerHTML = `Total Confirmed : ${global.TotalConfirmed}`;
 
+    td.innerHTML = `New Deaths : ${global.NewDeaths}`;
+    tdd.innerHTML = `Total Deaths : ${global.TotalDeaths}`;
 
+    tr.innerHTML = `New Recovered : ${global.NewRecovered}`;
+    trr.innerHTML = `Total Recovered : ${global.TotalRecovered}`;
 
-let table = document.getElementById('table');
+    let table = document.getElementById("table");
 
-for( i=1; i<249; i++){
- 
- let x = table.insertRow();
- 
- x.insertCell(0);
- table.rows[i].cells[0].innerHTML = countries[i-1].Country;
- table.rows[i].cells[0].style.background = '#0275d8';
- 
- x.insertCell(1);
- table.rows[i].cells[1].innerHTML = countries[i-1].NewConfirmed;
- table.rows[i].cells[1].style.background = '#f0ad4e';
- 
- x.insertCell(2);
- table.rows[i].cells[2].innerHTML = countries[i-1].NewDeaths;
- table.rows[i].cells[2].style.background = '#d9534f';
- 
- x.insertCell(3);
- table.rows[i].cells[3].innerHTML = countries[i-1].NewRecovered;
- table.rows[i].cells[3].style.background = '#5cb85c';
- 
- x.insertCell(4);
- table.rows[i].cells[4].innerHTML = countries[i-1].TotalConfirmed;
- table.rows[i].cells[4].style.background = '#f0ad4e';
- 
- x.insertCell(5);
- table.rows[i].cells[5].innerHTML = countries[i-1].TotalDeaths;
- table.rows[i].cells[5].style.background = '#d9534f';
- 
- x.insertCell(6);
- table.rows[i].cells[6].innerHTML = countries[i-1].TotalRecovered;
- table.rows[i].cells[6].style.background = '#5cb85c';
- 
-}
-  
+    for (let i = 1; i < 249; i++) {
+      let x = table.insertRow();
+
+      x.insertCell(0);
+      table.rows[i].cells[0].innerHTML = countries[i - 1].Country;
+      table.rows[i].cells[0].style.background = "#0275d8";
+
+      x.insertCell(1);
+      table.rows[i].cells[1].innerHTML = countries[i - 1].NewConfirmed;
+      table.rows[i].cells[1].style.background = "#f0ad4e";
+
+      x.insertCell(2);
+      table.rows[i].cells[2].innerHTML = countries[i - 1].NewDeaths;
+      table.rows[i].cells[2].style.background = "#d9534f";
+
+      x.insertCell(3);
+      table.rows[i].cells[3].innerHTML = countries[i - 1].NewRecovered;
+      table.rows[i].cells[3].style.background = "#5cb85c";
+
+      x.insertCell(4);
+      table.rows[i].cells[4].innerHTML = countries[i - 1].TotalConfirmed;
+      table.rows[i].cells[4].style.background = "#f0ad4e";
+
+      x.insertCell(5);
+      table.rows[i].cells[5].innerHTML = countries[i - 1].TotalDeaths;
+      table.rows[i].cells[5].style.background = "#d9534f";
+
+      x.insertCell(6);
+      table.rows[i].cells[6].innerHTML = countries[i - 1].TotalRecovered;
+      table.rows[i].cells[6].style.background = "#5cb85c";
+    }
+  }
 }
 
-fetch("https://api.covid19api.com/summary").then((apiData)=>{
-  return apiData.json();
-}).then((data)=>{
-  const coronaData = data;
-  runFetch(coronaData);
-})
-  
-  
-  
-  
-  
-}
+const searchFun = () => {
+  let input = document.getElementById("myInput").value.toUpperCase();
+  let table = document.getElementById("table");
+  let tr = table.getElementsByTagName("tr");
 
+  for (let i = 1; i < tr.length; i++) {
+    let td = tr[i].getElementsByTagName("td")[0];
 
+    if (td) {
+      let tdValue = td.textContent.toUpperCase();
 
-
-
+      if (tdValue.indexOf(input) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+};
